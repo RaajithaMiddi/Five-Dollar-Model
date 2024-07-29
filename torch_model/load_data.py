22c8cf06dd3d52e10023f3732e81f4c92cb98345
Kelsi Blauvelt <kelsi@rebelliondefense.com>
2024-07-28 21:22:36 -0700

    fix loss train val

--- torch_model/load_data.py
+++ torch_model/load_data.py
@@ -1,7 +1,6 @@
-import torch
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
 import numpy as np
+import torch
+from torch.utils.data import DataLoader, Dataset
 
 
 class NumpyDataset(Dataset):
@@ -20,11 +19,18 @@ class NumpyDataset(Dataset):
         self.images = images
         self.labels = labels
         self.embeddings = embeddings
-        self.color_palette = data["color_palette"]
-        self.color_palette_rgb = [
-            tuple(int(color[i : i + 2], 16) for i in (1, 3, 5))
-            for color in self.color_palette
-        ]
+        try:
+            self.color_palette = data["color_palette"]
+            self.color_palette_rgb = [
+                tuple(int(color[i : i + 2], 16) for i in (1, 3, 5))
+                for color in self.color_palette
+            ]
+        except KeyError:
+            self.color_palette = data["hex_colors"]
+            self.color_palette_rgb = [
+                tuple(int(color[i : i + 2], 16) for i in (1, 3, 5))
+                for color in self.color_palette
+            ]
 
     def __len__(self):
         return len(self.images)
@@ -34,7 +40,8 @@ class NumpyDataset(Dataset):
         labels = self.labels[idx]
         embeddings = torch.tensor(self.embeddings[idx], dtype=torch.float32)
         return img, labels, embeddings
-    
+
+
 def load_data(path, batch_size):
     dataset = NumpyDataset(path)
     color_palette = np.array(dataset.color_palette_rgb) / 255.0
--- torch_model/training.py
+++ torch_model/training.py
@@ -1,23 +1,20 @@
 import os
 
 import torch
+from load_data import load_data
+from PIL import Image
 from torch import nn, optim
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from tqdm import tqdm
-from PIL import Image
+from visualization import plot_loss_acc
 
 from torch_model import Generator
-from load_data import load_data
 from utils import decode_image_batch, image_grid
-from visualization import plot_loss_acc
 
 
 class GeneratorModule(nn.Module):
     def __init__(
-        self,
-        device: torch.device,
-        generator: Generator,
-        palette: Image.Image
+        self, device: torch.device, generator: Generator, palette: Image.Image
     ):
         super().__init__()
         self.device = device
@@ -39,7 +36,8 @@ class GeneratorModule(nn.Module):
         pred_out = self.generator(images, embeddings)
         true_classes = images.argmax(dim=-1)
         loss = self.loss_fn(torch.log(pred_out), true_classes)
-        return loss
+        # acc = torch.sum(true_classes == pred_out.max(1)).item() / true_classes.size(0)
+        return loss  # , acc
 
     def eval_step(self, batch, epoch: int):
         images, embeddings = self.process_batch(batch)
@@ -52,34 +50,42 @@ class GeneratorModule(nn.Module):
         results_grid = image_grid([input_images, pred_images])
         image_classes = images.argmax(dim=-1)
         loss = self.loss_fn(torch.log(pred_out), image_classes)
+        # acc = torch.sum(pred_out == image_classes).item() / len(image_classes)
+        if os.path.exists("debug_images") is False:
+            os.makedirs("debug_images")
         results_grid.save(os.path.join("debug_images", f"results_epoch_{epoch}.png"))
-        return loss
+        return loss  # , acc
+
 
 def train(file_path, hyperparameters, device):
-    epochs = hyperparameters['epochs']
-    batch_size = hyperparameters['batch_size']
-    lr = hyperparameters['learning_rate']
+    epochs = hyperparameters["epochs"]
+    batch_size = hyperparameters["batch_size"]
+    lr = hyperparameters["learning_rate"]
 
+    # train_set, test_set, palette = load_data("datasets/sprite_gpt4aug.npy", batch_size)
     train_set, test_set, palette = load_data(file_path, batch_size)
 
     model = GeneratorModule(device, Generator(device), palette)
 
     train_loss = []
+    train_acc = []
     eval_loss = []
+    val_acc = []
 
     model.to(device)
 
     optimizer = optim.Adam(model.parameters(), lr)
     scheduler = CosineAnnealingLR(optimizer, T_max=epochs, eta_min=0)
-
-    for epoch in tqdm(range(epochs), desc ="Training on epoch"):
+    for epoch in tqdm(range(epochs), desc="Training on epoch"):
 
         batch_loss = 0
+        acc_total = 0
         for j, batch in enumerate(train_set):
             optimizer.zero_grad()
 
             loss = model.training_step(batch)
             batch_loss += loss.item()
+            # acc_total += acc
 
             loss.backward()
             grads = [
@@ -97,21 +103,29 @@ def train(file_path, hyperparameters, device):
             )
             optimizer.step()
             scheduler.step()
-        train_loss.append(batch_loss/batch_size)
 
-        eval_every = 5
+        train_loss.append(
+            batch_loss / len(train_set)
+        )  # should be len training set / batch_size)
+        # train_acc.append(acc_total / len(train_set))
+
+        eval_every = 1
         if epoch % eval_every == 0:
             for j, batch in enumerate(test_set):
-                print("Running eval..")
                 val_loss = model.eval_step(batch, epoch)
                 eval_loss.append(val_loss.item())
+                # val_acc.append(val_acc)
+                print(f"Validation loss: {val_loss.item()}")
                 break
 
     torch.save(model.state_dict(), "five_dollar_model.pt")
     plot_loss_acc(train_loss, eval_loss)
 
+
 def _check_cuda_memory(device_id):
-    return torch.cuda.get_device_properties(device_id).total_memory - torch.cuda.memory_allocated(device_id)
+    return torch.cuda.get_device_properties(
+        device_id
+    ).total_memory - torch.cuda.memory_allocated(device_id)
 
 
 def select_best_device(device=None):
@@ -123,13 +137,15 @@ def select_best_device(device=None):
         return torch.device(device)
 
     if torch.cuda.is_available():
-        best_gpu = max(range(torch.cuda.device_count()), key=lambda i: _check_cuda_memory(i))
-        return torch.device(f'cuda:{best_gpu}')
+        best_gpu = max(
+            range(torch.cuda.device_count()), key=lambda i: _check_cuda_memory(i)
+        )
+        return torch.device(f"cuda:{best_gpu}")
 
-    if hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
-        return torch.device('mps')
+    if hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        return torch.device("mps")
 
-    return torch.device('cpu')
+    return torch.device("cpu")
 
 
 def _create_folders_if_not_exist(folder_path):
@@ -153,17 +169,15 @@ def create_directories():
 
 if __name__ == "__main__":
     device = select_best_device()
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using {device} device...")
 
     create_directories()
 
-    print(f"Using {device} device...")
     file_path = "datasets/sprite_gpt4aug.npy"
 
-    hyperparameters = {
-        "epochs": 100,
-        "batch_size": 256,
-        "learning_rate": 0.0005
-    }
+    hyperparameters = {"epochs": 100, "batch_size": 256, "learning_rate": 0.0005}
 
+    epochs = 2
+    batch_size = 256
+    lr = 0.0005
     train(file_path, hyperparameters, device)
