cfa12b74e234174a2a79da123e42ac38db30c09f
Henry Mei <me@henrymei.com>
2024-07-28 22:36:14 -0700

    add level OHE to quantizer

--- utils/images.py
+++ utils/images.py
@@ -2,6 +2,26 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 
+def _oht_images(images, num_levels=16):
+    """
+    Converts quantized RGB images to OHT quantized ndarray of images.
+    
+    :param images: ndarray of n images by lxw size with value equal to a quantized level
+    :param levels: the number of levels
+    :return: an ndarray that adds an additional dimension that is the OHT vector of levels
+    """
+    n, l, w = images.shape
+    # Initialize the one-hot encoded image
+    oht_images = np.zeros((n, l, w, num_levels), dtype=np.uint8)
+
+    # Populate the one-hot encoded image
+    for i in range(n):
+        for j in range(num_levels):
+            oht_images[i, :, :, j] = (images[i, :, :] == j).astype(np.uint8)
+
+    return oht_images
+
+
 def quantize_images(images, n_colors=16, model=None, seed=0):
     """
 
@@ -24,7 +44,7 @@ def quantize_images(images, n_colors=16, model=None, seed=0):
     quantized_pixels = model.predict(pixels)
     quantized_images = np.reshape(quantized_pixels, (n, l, w))
 
-    return quantized_images, rgb_colors, model
+    return _oht_images(quantized_images), rgb_colors, model
 
 
 def convert_images_to_rgb(images, centroids):
